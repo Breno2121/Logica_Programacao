e49fc3afbae5b898633eddf0e6d1c0eae04d6894
Breno2121 <josebrenosilva0@gmail.com>
2024-06-22 21:04:45 -0300

    22-06

--- 03-estrutura-repeticao/src/exercicio8.js
+++ 03-estrutura-repeticao/src/exercicio8.js
@@ -1,10 +1,17 @@
 import leia from 'readline-sync'
 
-var c1 = 0;
+var c1 = 1;
+var npares = 0;
+var nimpar = 0;
 
 while(c1 <= 6){
-    var numero = leia.question("Digite um numero: ");
+    var numero = leia.questionInt("Digite um numero: ");
+    if(numero % 2 == 0){
+        npares += numero;
+    }else {
+        nimpar += numero;
+    }
     c1++;
-    
 }
-
+console.log(nimpar);
+console.log(npares);
---
+++ 03-estrutura-repeticao/src/exercicio9.js
@@ -0,0 +1,25 @@
+import leia from 'readline-sync'
+
+    {
+if(c1 === 1){
+    var n1 = numero;
+    }else if(c1 === 2){
+        var n2 = numero;
+    }else if(c1 === 3){
+        var n3 = numero;
+    }else if(c1 === 4){
+        var n4 = numero;
+    }else if(c1 === 5){
+        var n5 = numero;
+    }else{
+        var n6 = numero;
+    }
+    c1++;
+}
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+
