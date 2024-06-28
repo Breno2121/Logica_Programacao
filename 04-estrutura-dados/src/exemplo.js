d4a4e91b740c7d33e2b4ea556c56de7674d81021
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2024-06-27 21:54:02 -0300

    27-06

---
+++ 04-estrutura-dados/src/exemplo.js
@@ -0,0 +1,10 @@
+import leia from 'readline-sync'
+
+
+var lista = [["Brenin", 23, "lindo", true,"Brenin", 23], ["lindo", true,"Brenin", 23, "lindo", true]];
+
+for(var i = 0;i < 6; i++){
+    for(var j = 0; j < lista.length; j++){
+    console.log(lista[i]);
+    }
+}
